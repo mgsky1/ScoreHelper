4171eff0882de119e2a940124948ad4b4f4faa99
mgsky1 <563044462@qq.com>
2018-02-16 16:28:04 +0800

    upgrade
    redesign the code

--- README.md
+++ README.md
@@ -5,11 +5,20 @@
 
 ## Note
 > * 这是第一版，运行后将会把结果打印在控制台
+<<<<<<< HEAD
+> * 程序会在脚本当前目录下生成验证码文件，并自动打开图片，需要手动输入
+> * 用户只要在第一次运行时输入相关信息，后期则不需输入
+> * bin目录下的exe可执行文件可以直接运行，**不依赖Python运行库**
+> * 如果Windows下exe文件运行错误的话请下载[Visual C++ Redistributable](https://www.microsoft.com/zh-cn/download/details.aspx?id=48145)**32位**版本
+> * 为了取得更好的显示效果，建议Windows下使用**PowerShell**运行，运行方式为：切换到bin目录，输入 `./三明学院成绩查询助手`
+> * 源码运时需要Python3以上版本，依赖[BeautifulSoup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/index.zh.html)、[PrettyTable](https://pypi.python.org/pypi/PrettyTable)和[PIL](http://www.pythonware.com/products/pil/)
+=======
 > * 程序会在脚本当前目录下生成验证码文件，需要手动输入
 > * 如果屏幕不够大的话，可能会有错位，sorry~后期修复
 > * 源码中的学号、密码替换成自己的学号、密码，源码中的两个不完整的链接可使用[Google Chrome](https://www.google.cn/chrome/)抓包自行获取，每人的姓名编码都不一样
 > * 运行时需要Python3以上版本，依赖[BeautifulSoup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/index.zh.html)
+>>>>>>> origin/master
 
 ## Screen Shots
 
-![](http://xxx.fishc.com/album/201802/15/174422gshp0hdthjhk3hvo.png)
+![](http://xxx.fishc.com/album/201802/16/160442pz5gyg8pg58yskr5.png)
--- src/getScore.py
+++ src/getScore.py
@@ -1,79 +1,146 @@
+'''
+三明学院成绩查询助手V1.0
+Coded By Martin Huang
+2018.2.16
+'''
 import re
 import urllib.request
 import urllib.parse
 import http.cookiejar
 import bs4
+import getpass
+import pickle
 from bs4 import BeautifulSoup
+from prettytable import PrettyTable
+from PIL import Image
 
 #准备Cookie和opener，因为cookie存于opener中，所以以下所有网页操作全部要基于同一个opener
 cookie = http.cookiejar.CookieJar()
 opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie))
-#构造表单
-params = {
-    'txtUserName' : '学号',
-    'Textbox1' : '',
-    'Textbox2': '密码',
-    'RadioButtonList1':'学生',
-    'Button1' : '',
-    'lbLanguage':'',
-    'hidPdrs':'',
-    'hidsc':'',
-}
 
-#获取ViewState
-response = urllib.request.urlopen('http://218.5.241.21')
-html = response.read().decode('gb2312')
-viewstate = re.search('<input type="hidden" name="__VIEWSTATE" value="(.+?)"',html)
-params['__VIEWSTATE'] = viewstate.group(1)
-
-#获取验证码
-res = opener.open('http://218.5.241.21/checkcode.aspx').read()
-with open('code.jpg','wb') as file:
-    file.write(res)
-vcode = input('请输入验证码：')
-params['txtSecretCode'] = vcode
-
-#尝试登陆
-loginurl = 'http://218.5.241.21/default2.aspx'
-data = urllib.parse.urlencode(params).encode('gb2312')
-response = opener.open(loginurl,data)
-
-#构建查询全部成绩表单
-params = {
-    'ddlXN':'',
-    'ddlXQ':'',
-    'txtQSCJ':'0',
-    'txtZZCJ':'100',
-    'Button1':'在校学习成绩',
-}
-
-#构造Request对象，填入Header，防止302跳转，获取新的View_State
-req = urllib.request.Request('http://218.5.241.21/xscj.aspx?xh=学号&xm=姓名(编码)&gnmkdm=N121604')
-req.add_header('Referer','http://218.5.241.21/default2.aspx')
-req.add_header('Origin','http://218.5.241.21')
-response = opener.open(req)
-html = response.read().decode('gb2312')
-viewstate = re.search('<input type="hidden" name="__VIEWSTATE" value="(.+?)"',html)
-params['__VIEWSTATE'] = viewstate.group(1)
-
-#查询所有成绩
-req = urllib.request.Request('http://218.5.241.21/xscj.aspx?xh=学号&xm=姓名(编码)&gnmkdm=N121604',urllib.parse.urlencode(params).encode('gb2312'))
-req.add_header('Referer','http://218.5.241.21/default2.aspx')
-req.add_header('Origin','http://218.5.241.21')
-response = opener.open(req)
-soup = BeautifulSoup(response.read().decode('gb2312'),'html.parser')
-html = soup.find('table',class_='datelist')
-print('你的所有成绩如下：')
-
-#格式化宽度输出
-mat = "{:^20}\t"
-
-#根据DOM解析所要数据，首位的each是NavigatableString对象，其余为Tag对象
-for each in html:
-    if(type(each) == bs4.element.NavigableString):
-        pass
+#登陆
+def login():
+    #构造表单
+    params = {
+        'txtUserName' : sid,
+        'Textbox1' : '',
+        'Textbox2': spwd,
+        'RadioButtonList1':'学生',
+        'Button1' : '',
+        'lbLanguage':'',
+        'hidPdrs':'',
+        'hidsc':'',
+    }
+    #获取验证码
+    res = opener.open('http://218.5.241.21/checkcode.aspx').read()
+    with open('code.jpg','wb') as file:
+        file.write(res)
+    img = Image.open('code.jpg')
+    img.show()
+    vcode = input('请输入验证码：')
+    img.close()
+    params['txtSecretCode'] = vcode
+    #获取ViewState
+    response = urllib.request.urlopen('http://218.5.241.21')
+    html = response.read().decode('gb2312')
+    viewstate = re.search('<input type="hidden" name="__VIEWSTATE" value="(.+?)"',html)
+    params['__VIEWSTATE'] = viewstate.group(1)
+    #尝试登陆
+    loginurl = 'http://218.5.241.21/default2.aspx'
+    data = urllib.parse.urlencode(params).encode('gb2312')
+    response = opener.open(loginurl,data)
+    if response.geturl() == 'http://218.5.241.21/default2.aspx':
+        print('登陆失败，可能是姓名、学号、密码、验证码填写错误！')
+        return False
     else:
-        for item in each.contents:
-            if(item != '\n'):
-                print(mat.format(item.contents[0]),end='')
-        print('\n')
+        return True
+
+#获取成绩   
+def getScore():
+    #构造url
+    url = ''.join([
+            'http://218.5.241.21/xscj.aspx',
+            '?xh=',
+             sid,
+            '&xm=',
+            urllib.parse.quote(sname),
+            '&gnmkdm=N121604',
+        ])
+    #构建查询全部成绩表单
+    params = {
+        'ddlXN':'',
+        'ddlXQ':'',
+        'txtQSCJ':'0',
+        'txtZZCJ':'100',
+        'Button1':'在校学习成绩',
+    }
+    #构造Request对象，填入Header，防止302跳转，获取新的View_State
+    req = urllib.request.Request(url)
+    req.add_header('Referer','http://218.5.241.21/default2.aspx')
+    req.add_header('Origin','http://218.5.241.21')
+    req.add_header('User-Agent','Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36')
+    response = opener.open(req)
+    html = response.read().decode('gb2312')
+    viewstate = re.search('<input type="hidden" name="__VIEWSTATE" value="(.+?)"',html)
+    params['__VIEWSTATE'] = viewstate.group(1)
+    #查询所有成绩
+    req = urllib.request.Request(url,urllib.parse.urlencode(params).encode('gb2312'))
+    req.add_header('Referer','http://218.5.241.21/default2.aspx')
+    req.add_header('Origin','http://218.5.241.21')
+    response = opener.open(req)
+    soup = BeautifulSoup(response.read().decode('gb2312'),'html.parser')
+    html = soup.find('table',class_='datelist')
+    print('你的所有成绩如下：')
+    #指定要输出的列
+    outColumn = [1,3,4,6,7,8]
+    #用于标记是否是遍历第一行
+    flag = True
+    #根据DOM解析所要数据，首位的each是NavigatableString对象，其余为Tag对象
+    #遍历行
+    for each in html:
+        columnCounter = 0
+        column = []
+        if(type(each) == bs4.element.NavigableString):
+            pass
+        else:
+            #遍历列
+            for item in each.contents:
+                if(item != '\n'):
+                    if columnCounter in outColumn:
+                        #要使用str转换，不然陷入copy与deepcopy的无限递归
+                        column.append(str(item.contents[0]).strip())
+                    columnCounter += 1
+            if flag:
+                table = PrettyTable(column)
+                flag = False
+            else:
+                table.add_row(column)
+    print(table)
+
+if __name__ == '__main__':
+    try:
+        print('欢迎使用三明学院成绩查询助手！')
+        with open('uinfo.bin','rb') as file:
+            udick = pickle.load(file)
+            sname = udick['sname']
+            sid = udick['sid']
+            spwd = udick['spwd']
+        login()
+        getScore()
+    except FileNotFoundError:
+        print('这是你第一次使用，请按提示输入信息，以后可不必再次输入~')
+        sid = input('请输入学号：')
+        sname = input('请输入姓名：')
+        #隐藏密码
+        spwd = getpass.getpass('请输入密码：')
+        while(not login()):
+            sname = input('请输入姓名：')
+            sid = input('请输入学号：')
+            spwd = getpass.getpass('请输入密码：')
+        getScore()
+        udick = {'sname':sname,'sid':sid,'spwd':spwd}
+        file = open('uinfo.bin','wb')
+        pickle.dump(udick,file)
+        file.close()
+    finally:
+        input('Done！请按任意键退出')
